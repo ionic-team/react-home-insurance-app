3633865912ecbf3625bfc0a7d3cda69be2f624ad
Sean Perkins <sean@ionic.io>
2024-03-26 20:01:26 -0400

    feat: login page

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
 
-const container = document.getElementById('root');
+import "./index.css";
+
+const container = document.getElementById("root");
 const root = createRoot(container!);
 root.render(
   <React.StrictMode>
---
+++ src/pages/Login/index.tsx
@@ -0,0 +1,76 @@
+import { IonButton, IonContent, IonPage, IonText } from "@ionic/react";
+
+import "./style.css";
+
+export function Login() {
+  return (
+    <IonPage>
+      <IonContent>
+        <div className="hero">
+          <img className="hero-logo" src="/logo.svg" alt="Logo" />
+          <IonText color="light">
+            <h1 className="ion-heading-1 text-center">
+              Keep everything
+              <br />
+              in one place.
+            </h1>
+          </IonText>
+          <div className="vector-3"></div>
+          <div className="vector-2"></div>
+        </div>
+
+        <div className="ion-padding-horizontal">
+          <IonButton
+            shape="round"
+            fill="solid"
+            expand="block"
+            size="large"
+            color="primary"
+          >
+            Log in
+          </IonButton>
+          <IonButton
+            className="mt-2"
+            shape="round"
+            fill="solid"
+            expand="block"
+            size="large"
+            color="primary-light"
+          >
+            Sign up
+          </IonButton>
+          <div className="separator"></div>
+          <IonButton
+            className="social-btn"
+            expand="block"
+            fill="outline"
+            shape="round"
+            size="large"
+            color="primary-light"
+          >
+            <img className="mr-2" src="/assets/google.svg" alt="Google Logo" />
+            Continue with Google
+          </IonButton>
+          <IonButton
+            className="mt-2 social-btn"
+            expand="block"
+            fill="outline"
+            shape="round"
+            size="large"
+            color="primary-light"
+          >
+            <img
+              className="mr-2"
+              src="/assets/facebook.svg"
+              alt="Facebook Logo"
+            />
+            Continue with Facebook
+          </IonButton>
+          <div className="flex flex-row justify-center mt-5">
+            <IonButton fill="clear">Log in as Sample User</IonButton>
+          </div>
+        </div>
+      </IonContent>
+    </IonPage>
+  );
+}
